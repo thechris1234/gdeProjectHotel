fc30087f951e2b3d7e16d1ef62ca2eabbff87fe5
thechris <ludvi81@gmail.com>
2024-04-25 11:43:05 +0200

    Optimized code for PyCharm compatibility

--- szalloda.py
+++ szalloda.py
@@ -1,18 +1,22 @@
 from datetime import datetime, date
 from random import randint, choice
 
+
 class Szoba:
     def __init__(self, szobaszam: int, ar: int):
         self.szobaszam = szobaszam
         self.ar = ar
 
+
 class EgyagyasSzoba(Szoba):
     def __init__(self, szobaszam: int, ar: int = 100):
         super().__init__(szobaszam, ar)
 
+
 class KetagyasSzoba(Szoba):
     def __init__(self, szobaszam: int, ar: int = 100):
         super().__init__(szobaszam, ar)
+
 
 class Foglalas:
     ev = None
@@ -24,26 +28,27 @@ class Foglalas:
         self.erkezes = datetime.strptime(erkezes, '%Y.%m.%d').timestamp()
         self.tavozas = datetime.strptime(tavozas, '%Y.%m.%d').timestamp()
 
+
 class Szalloda:
     def __init__(self, szobamennyiseg: int, elorefoglalt: int):
         self.nev = "Gazdag ember"
         self.datum = date.today().strftime('%Y.%m.%d')
         self.ma = datetime.strptime(self.datum, '%Y.%m.%d').timestamp()
-        
+
         self.szobak = dict()
         self.foglalasok = []
-        
+
         if szobamennyiseg:
             self.szobakFeltoltese(szobamennyiseg)
-        
+
         if elorefoglalt:
             self.szobakLefoglalasa(elorefoglalt)
-    
+
     def szobakFeltoltese(self, mennyiseg: int):
         for _ in range(0, mennyiseg):
             egyagyas = bool(randint(0, 1))
             ar = 150 if egyagyas else 250
-            
+
             if egyagyas:
                 self.szobaHozzaadas(EgyagyasSzoba(randint(100, 199), ar))
             else:
@@ -52,15 +57,15 @@ class Szalloda:
     def szobaHozzaadas(self, szoba: Szoba):
         self.szobak[szoba.szobaszam] = {'ár': szoba.ar, 'típus': 'egyágyas' if isinstance(szoba, EgyagyasSzoba) else 'kétágyas'}
 
-    def elerhetoIdoszak(self, szobaszam: int, erkezes: int, tavozas: int):
+    def elerhetoIdoszak(self, szobaszam: int, erkezes: float, tavozas: float):
         if szobaszam not in self.szobak:
             print(f"Nincsen {szobaszam} számú szoba!")
             return False
-        
+
         if erkezes == tavozas:
             print(f"A(z) {szobaszam} számú foglaláson azonos érkezési és távozási nap van megadva!")
             return False
-        
+
         if erkezes > tavozas:
             print(f"A(z) {szobaszam} számú foglaláson az érkezési dátum később van, mint a távozási dátum!")
             return False
@@ -68,15 +73,16 @@ class Szalloda:
         if erkezes < self.ma or tavozas < self.ma:
             print(f"A(z) {szobaszam} számú foglalás nem lehetséges, mivel nem tudsz időt utazni!")
             return False
-        
-        if szobaszam in self.foglalasok:
-            foglalas = self.foglalasok.get(szobaszam)
+
+        for foglalas in self.foglalasok:
+            fSzobaszam = foglalas.get('szobaszam')
             fErkezes = foglalas.get('erkezes')
             fTavozas = foglalas.get('tavozas')
 
-            if erkezes <= fTavozas and tavozas >= fErkezes:
-                print(f"A(z) {szobaszam} számú szoba ekkor már FOGLALT!")
-                return False
+            if szobaszam == fSzobaszam:
+                if erkezes <= fTavozas and tavozas >= fErkezes:
+                    print(f"A(z) {szobaszam} számú szoba ekkor már FOGLALT!")
+                    return False
 
         return True
 
@@ -91,24 +97,24 @@ class Szalloda:
             tavozas = f"{tavozas[0]}.{str(tavozas[1]).rjust(2, '0')}.{str(tavozas[2]).rjust(2, '0')}"
 
             self.szobaLefoglalas(Foglalas(Szoba(szobaszam, 0), erkezes, tavozas), False)
-    
+
     def szobaLefoglalas(self, adat: Foglalas, visszajelzes: bool = True):
         szobaszam = adat.szoba.szobaszam
         erkezes = adat.erkezes
         tavozas = adat.tavozas
-        
+
         if not self.elerhetoIdoszak(szobaszam, erkezes, tavozas):
             return
 
-        self.foglalasok.append({'szobaszam': szobaszam, 'erkezes': erkezes , 'tavozas': tavozas})
-    
+        self.foglalasok.append({'szobaszam': szobaszam, 'erkezes': erkezes, 'tavozas': tavozas})
+
         erkezes = datetime.fromtimestamp(erkezes).strftime('%Y.%m.%d')
         tavozas = datetime.fromtimestamp(tavozas).strftime('%Y.%m.%d')
 
         if visszajelzes:
             print(f"Sikeres foglalás a {szobaszam} számú szobára {erkezes} és {tavozas} között!")
         return adat.szoba.ar
-    
+
     def foglalasLemondas(self, szobaszam: int, erkezes: str, tavozas: str):
         erkezes = datetime.strptime(erkezes, '%Y.%m.%d').timestamp()
         tavozas = datetime.strptime(tavozas, '%Y.%m.%d').timestamp()
@@ -120,25 +126,28 @@ class Szalloda:
 
             if fszobaszam == szobaszam and ferkezes == erkezes and ftavozas == tavozas:
                 self.foglalasok.remove(foglalas)
-                print(f"A {szobaszam} számú szoba foglalása {datetime.fromtimestamp(erkezes).strftime('%Y.%m.%d')} - {datetime.fromtimestamp(tavozas).strftime('%Y.%m.%d')} között sikeresen törölve!")
+                print(
+                    f"A {szobaszam} számú szoba foglalása {datetime.fromtimestamp(erkezes).strftime('%Y.%m.%d')} - {datetime.fromtimestamp(tavozas).strftime('%Y.%m.%d')} között sikeresen törölve!")
                 return
 
-        print(f"A {szobaszam} számú szoba foglalása {datetime.fromtimestamp(erkezes).strftime('%Y.%m.%d')} - {datetime.fromtimestamp(tavozas).strftime('%Y.%m.%d')} között nem található!")
-    
+        print(
+            f"A {szobaszam} számú szoba foglalása {datetime.fromtimestamp(erkezes).strftime('%Y.%m.%d')} - {datetime.fromtimestamp(tavozas).strftime('%Y.%m.%d')} között nem található!")
+
     def foglalasokListazasa(self):
-        print(f"A(z) {szalloda.nev} szálloda foglalásainak listája:")
-        
+        print(f"A(z) {szalloda.nev} szálloda foglalásainak listája: ")
+
         for foglalas in self.foglalasok:
-            print(f"A {foglalas['szobaszam']} szoba le van foglalva {datetime.fromtimestamp(foglalas['erkezes']).strftime('%Y.%m.%d')} - {datetime.fromtimestamp(foglalas['tavozas']).strftime('%Y.%m.%d')} között")
+            print(
+                f"A {foglalas['szobaszam']} szoba le van foglalva {datetime.fromtimestamp(foglalas['erkezes']).strftime('%Y.%m.%d')} - {datetime.fromtimestamp(foglalas['tavozas']).strftime('%Y.%m.%d')} között")
 
 
-#Felhasználói Interfész
+# Felhasználói Interfész
 class Porta:
-    def __init__(self, szalloda = Szalloda):
+    def __init__(self, szalloda: object = Szalloda):
         self.szallodaPorta()
-        
+
     def szobakListazasa(self):
-        print(f"\nA(z) {szalloda.nev} szálloda szobáinak listája:")
+        print(f"\nA(z) {szalloda.nev} szálloda szobáinak listája: ")
 
         for szobaszam, szoba in szalloda.szobak.items():
             print(f"{szobaszam} - {szoba['típus']} szoba, ára: {szoba['ár']} Ft")
@@ -153,13 +162,13 @@ class Porta:
             print("3 - Foglalások listázása")
             print("4 - Szobák listázása")
             print("5 - Kilépés")
-            
+
             try:
                 valasz = input("Válassz a menüpontok közül: ")
             except KeyboardInterrupt:
                 print("\nElhagytad a portát.")
                 break
-            
+
             if valasz == "1":
                 try:
                     szobaszam = int(input(f"Add meg a szoba számát: "))
@@ -169,14 +178,14 @@ class Porta:
                 except KeyboardInterrupt:
                     print("\nElhagytad a portát.")
                     break
-                
+
                 try:
                     erkezes = input("Add meg az érkezésed dátumát (pl.: 2024.04.24): ")
                 except KeyboardInterrupt:
                     print("\nElhagytad a portát.")
                     break
-                
-                if self.datumellenorzes(erkezes) == False:
+
+                if not self.datumEllenorzes(erkezes):
                     print("Hibás dátum formátum!")
                     continue
 
@@ -185,8 +194,8 @@ class Porta:
                 except KeyboardInterrupt:
                     print("\nElhagytad a portát.")
                     break
-                
-                if self.datumellenorzes(tavozas) == False:
+
+                if not self.datumEllenorzes(tavozas):
                     print("Hibás dátum formátum!")
                     continue
 
@@ -201,27 +210,27 @@ class Porta:
                 except KeyboardInterrupt:
                     print("\nElhagytad a portát.")
                     break
-                
+
                 try:
                     erkezes = input("Add meg az érkezési dátumot (pl.: 2024.04.24): ")
                 except KeyboardInterrupt:
                     print("\nElhagytad a portát.")
                     break
-                
-                if self.datumellenorzes(erkezes) == False:
+
+                if not self.datumEllenorzes(erkezes):
                     print("Hibás dátum formátum!")
                     continue
-                
+
                 try:
                     tavozas = input("Add meg a távozási dátumot (pl.: 2024.04.25): ")
                 except KeyboardInterrupt:
                     print("\nElhagytad a portát.")
                     break
-                
-                if self.datumellenorzes(tavozas) == False:
+
+                if not self.datumEllenorzes(tavozas):
                     print("Hibás dátum formátum!")
                     continue
-                
+
                 szalloda.foglalasLemondas(szobaszam, erkezes, tavozas)
 
             elif valasz == "3":
@@ -234,12 +243,13 @@ class Porta:
                 print("Viszont látásra!")
                 break
 
-    def datumellenorzes(self, datum: str):
+    def datumEllenorzes(self, datum: str):
         try:
             datetime.strptime(datum, '%Y.%m.%d')
             return True
         except ValueError:
             return False
 
-szalloda = Szalloda(szobamennyiseg = 3, elorefoglalt = 5)
+
+szalloda = Szalloda(szobamennyiseg=3, elorefoglalt=5)
 porta = Porta(szalloda)
